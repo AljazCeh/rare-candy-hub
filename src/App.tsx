4bd5cf8f608ff99b38ff93d2d09b420d099a579f
akralj563 <akralj563@gmail.com>
2026-01-29 08:46:40 +0100

    Poprava App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+
 import Index from "./pages/Index";
 import ONas from "./pages/ONas";
 import Storitve from "./pages/Storitve";
@@ -19,16 +20,16 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/o-nas" element={<ONas />} />
-            <Route path="/storitve" element={<Storitve />} />
-            <Route path="/reference" element={<Reference />} />
-            <Route path="/kontakt" element={<Kontakt />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
+
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/o-nas" element={<ONas />} />
+          <Route path="/storitve" element={<Storitve />} />
+          <Route path="/reference" element={<Reference />} />
+          <Route path="/kontakt" element={<Kontakt />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+
       </TooltipProvider>
     </QueryClientProvider>
   </HelmetProvider>
